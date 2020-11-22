ad50f721eccaa43383bc3c309023032659e61b0f
davide <davide@davide-HP-EliteBook-2560p>
2020-11-22 10:53:16 +0100

    Initial commit

---
+++ src/jhashcodetest/JHashCodeTest.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jhashcodetest;
+
+import it.dsestili.jhashcode.core.Core;
+import java.io.File;
+import java.security.MessageDigest;
+import org.apache.commons.codec.digest.DigestUtils;
+
+/**
+ *
+ * @author davide
+ */
+public class JHashCodeTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try
+        {
+            if(args.length != 2)
+            {
+                    System.out.println("Usage: param 1: file path, param 2: algorithm");
+                    return;
+            }
+
+            String fileName = args[0];
+            String algorithm = args[1];
+
+            if(fileName == null || fileName.isEmpty())
+            {
+                    System.out.println("Blank file name!");
+                    return;
+            }
+
+            System.out.println("File name: " + fileName);
+            File file = new File(fileName);
+
+            if(!file.exists())
+            {
+                    System.out.println("File does not exist!");
+                    return;
+            }
+
+            if(!file.isFile())
+            {
+                    System.out.println("Is not a file!");
+                    return;
+            }
+
+            Core core = new Core(file, algorithm);
+            String hash1 = core.generateHash();
+            
+            System.out.println("Hash (Core): " + hash1);
+            
+            DigestUtils digestUtils = new DigestUtils(MessageDigest.getInstance(algorithm));
+            String hash2 = digestUtils.digestAsHex(file);
+            
+            System.out.println("Hash (DigestUtils): " + hash2);
+            
+            if(hash1.equalsIgnoreCase(hash2))
+            {
+                System.out.println("Same result");
+            }
+            else
+            {
+                System.out.println("Different result");
+            }
+        }
+        catch(Throwable t)
+        {
+            t.printStackTrace();
+        }
+    }
+}
